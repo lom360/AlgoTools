2ae00020e6583ffdb9d120d4f33653eeacc76e4d
Lom Thin <sarom_thin@outlook.com>
2023-01-09 03:01:10 -0800

    Completed week1 assignments in C++

--- week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
+++ week1_programming_challenges/2_maximum_pairwise_product/max_pairwise_product.cpp
@@ -16,14 +16,35 @@ int MaxPairwiseProduct(const std::vector<int>& numbers) {
     return max_product;
 }
 
+long long FastPairwiseProduct(const std::vector<long long>& numbers) {
+    int n = numbers.size();
+
+    int maxIndex1 = -1;
+    for(int i = 0; i < n; i++){
+        if(numbers[i] > numbers[maxIndex1] || maxIndex1 == -1) {
+	    maxIndex1 = i;
+	}
+    }
+
+    int maxIndex2 = -1;
+    for(int j = 0; j < n; j++) {
+        if((numbers[j] > numbers[maxIndex2] || maxIndex2 == -1) && j != maxIndex1) {
+	    maxIndex2 = j;
+	}
+    }
+
+    return ((long long) numbers[maxIndex1]) * numbers[maxIndex2];
+}
+
 int main() {
     int n;
     std::cin >> n;
-    std::vector<int> numbers(n);
+    std::vector<long long> numbers(n);
     for (int i = 0; i < n; ++i) {
         std::cin >> numbers[i];
     }
 
-    std::cout << MaxPairwiseProduct(numbers) << "\n";
+    //std::cout << MaxPairwiseProduct(numbers) << "\n";
+    std::cout << FastPairwiseProduct(numbers) << "\n";
     return 0;
 }
