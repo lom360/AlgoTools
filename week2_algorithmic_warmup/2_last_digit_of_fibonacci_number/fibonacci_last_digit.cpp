d763c42243c8536312151298a2ca94b66ba24840
Lom Thin <sarom_thin@outlook.com>
2023-01-10 00:13:07 -0800

    Completed last digit fibonacci problem.

--- week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
+++ week2_algorithmic_warmup/2_last_digit_of_fibonacci_number/fibonacci_last_digit.cpp
@@ -16,20 +16,18 @@ int get_fibonacci_last_digit_naive(int n) {
     return current % 10;
 }
 
-int fibonacci(int n) {
-    if(n == 0) {
-        return 0;
-    }
-    else if(n == 1) {
-        return 1;
-    }
-    else {
-        return fibonacci(n-1) + fibonacci(n-2);
-    }
-}
-
 int get_fibonacci_last_digit_fast(int n) {
-    return fibonacci(n) % 10;
+    if(n <= 1)
+        return n;
+    int previous = 0;
+    int current = 1;
+
+    for(int i = 0; i < n - 1; i++) {
+        int temp_prev = previous;
+        previous = current;
+        current = (temp_prev + previous) % 10;
+    }
+    return current;
 }
 
 int main() {
